712c2039ff31cd6dc1d24e0171fe0ec26e3d85f5
JiorMajor <jiormajor@gmail.com>
2015-06-03 09:13:22 +0800

    Adding Methods
    CRUD features method added.

--- src/dao/StudentDataAccess.java
+++ src/dao/StudentDataAccess.java
@@ -1,28 +1,75 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import model.Student;
 
 public class StudentDataAccess {
-	
-	
-	public int createStudent(Student s) {
-		return 0;
+
+	private static Connection connection;
+	private static Statement statement;
+	private static ResultSet rs;
+
+	public static void openConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/demo", "root", "");
+			statement = connection.createStatement();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
-	
+
+	public static void closeConnection() {
+		try {
+			statement.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int createStudent(Student s) throws SQLException {
+		openConnection();
+		String insert = "INSERT INTO student(id, name, nick, nirc, mark, dob) VALUES "
+				+ "("
+				+s.getId()
+				+",\""+s.getName()
+				+"\",\""
+				+s.getNick()
+				+"\",\""
+				+s.getNirc()
+				+"\","
+				+s.getMark()
+				+","
+				+s.getDob()+
+				");";
+		int result = statement.executeUpdate(insert);
+		closeConnection();		
+		return result;
+	}
+
 	public Student readOneStudent(Student s) {
 		return null;
 	}
-	
+
 	public ArrayList<Student> readAllStudent() {
 		return null;
 	}
-	
+
 	public int updateStudent(Student s) {
 		return 0;
 	}
-	
+
 	public int deleteStudent(Student s) {
 		return 0;
 	}
